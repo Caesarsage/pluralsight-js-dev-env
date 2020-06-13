5b08effb7c9810b2f47504ced9181f4349cbd903
Destiny Erhabor <destinterhabor6@gmail.com>
2020-06-13 01:37:22 +0100

    configuring Webpack

--- buildScript/startMessage.js
+++ buildScript/startMessage.js
@@ -1,3 +1,3 @@
 import chalk from 'chalk';
 
-console.log(chalk.green("Sarting app in dev mode ...")) //run this before the web server starts up. setup in packagejon => script
+console.log(chalk.yellow("Sarting app in dev mode ...")) //run this before the web server starts up. setup in packagejon =>  
---
+++ webpack.config.dev.js
@@ -0,0 +1,30 @@
+import webpack from 'webpack';
+import path from 'path';
+
+export default{
+    debug: true,
+    devtool: 'inline-source-map', //
+    noInfo: false, //webpace display
+    //define the empty point
+    entry: [
+        path.resolve(__dirname, 'src/index')
+    ],
+    //define target : web or node or electron etc
+    target: 'web',
+    //define output => where to create our bundle in memory
+    //create a path / build process that generate file for production
+    output: {
+        path: path.resolve(__dirname, 'src'),
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    //[optional] define a plugin such as hot reloading , catching error
+    plugins:[],
+    //Define file type by defining loaders
+    module: {
+        loaders: [
+            {test: /\.js$/, exclude: /node_module/, loaders: ['babel']},
+            {test: /\.css$/, loaders: ['style','css']}
+        ]
+    }
+};
