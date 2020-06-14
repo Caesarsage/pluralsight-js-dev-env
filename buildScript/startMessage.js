de3f04eaeac50e58068e633f9ff0dd3111326751
Destiny Erhabor <destinterhabor6@gmail.com>
2020-06-14 05:47:42 +0100

    setting up lint and lint watch to automatically watch our file

--- buildScript/startMessage.js
+++ buildScript/startMessage.js
@@ -1,3 +1,5 @@
 import chalk from 'chalk';
 
-console.log(chalk.yellow("Sarting app in dev mode ...")) //run this before the web server starts up. setup in packagejon =>  
+
+console.log(chalk.yellow("Sarting app in dev mode ...")) //eslint-disable-line no-console 
+//run this before the web server starts up. setup in packagejon =>  
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import './index.css';
 
 import numeral from 'numeral';
 
+/* eslint-disable no-console*/
+
 const courseValue = numeral(1000).format('$0,0.00');
 debugger //line where we want the breakpoint to hit
 console.log(`I would pa ${courseValue} fpr this awesome course`);
