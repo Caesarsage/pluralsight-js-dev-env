c55c7ac4c129de9b0d62b722d8b5fff64c3c162f
Destiny Erhabor <destinterhabor6@gmail.com>
2020-06-14 07:24:10 +0100

    Test to JsDom

--- src/index.test.js
+++ src/index.test.js
@@ -1,8 +1,27 @@
 import {expect} from "chai";
 import { describe } from "mocha";
+import jsdom from 'jsdom';
+import fs from 'fs';
 
 describe('Our first test', ()=>{
     it('should pass', ()=>{
+        //asertion with chai
         expect(true).to.equal(true)
+    });
+});
+
+//test to jsDom
+
+describe('index.html', ()=>{
+    it('should say hello', (done)=>{
+        //get the element to assert
+        const index = fs.readFileSync('./src/index.html', "utf-8");
+        jsdom.env(index, function (err, window) {
+            const h1 = window.document.getElementByTagName('h1')[0];
+            //asertion
+            expect(h1.innerHTML).to.equal("Hello world!!!");
+            done(); //add
+            window.close();        
+        });
     });
 });
